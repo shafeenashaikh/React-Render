ad2f6a5c9cda1ed850005f72ee17cbfb8b204463
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-09-13 12:30:26 +0530

    Parent child rendring

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { ObjectUseState } from './Component/ImmutableState/ObjectUseState';
+import { Parent } from './Component/parent child/Parent';
 import { UseReducer } from './Component/UseReducer/UseReducer';
 import { UseState } from './Component/UseState/UseState';
 
@@ -7,7 +8,8 @@ function App() {
   console.log('UseState Render')
   return (
     <div className="App">
-      <ObjectUseState/>
+      <Parent/>
+      {/* <ObjectUseState/> */}
       {/* <UseState/> */}
       {/* <UseReducer/> */}
     </div>
